cabeb5b9594d2b73e1cb1b542eddaffb3534bc50
0x01001111 <axel.orrenius@hotmail.com>
2023-10-24 14:35:00 +0200

    lite fler knappar

--- client/src/components/courseForum.tsx
+++ client/src/components/courseForum.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
 
 interface RouteParams {
   courseId: string; // Define the type of courseId here
@@ -13,58 +14,70 @@ const mockThreads = [
     id: 1,
     title: 'Thread 1',
     content: 'This is the content of Thread 1.',
-    likesComments: '5 comments, 10 likes'
+    likesComments: '5 comments, 10 likes',
   },
   {
     id: 2,
     title: 'Thread 2',
     content: 'This is the content of Thread 2.',
-    likesComments: '5 comments, 10 likes'
+    likesComments: '5 comments, 10 likes',
   },
   {
     id: 3,
     title: 'Thread 3',
     content: 'This is the content of Thread 3.',
-    likesComments: '5 comments, 10 likes'
+    likesComments: '5 comments, 10 likes',
   },
 ];
 
 function CourseDetail() {
   const { courseId } = useParams<RouteParams>();
 
-  // Now courseId should be recognized as a string.
-  const addThread = () => {
-    // Implement the logic to enter a new thread here
-    console.log('Entering a new thread');
+  const [showModal, setShowModal] = useState(false);
+  const [newThread, setNewThread] = useState('');
+  const [threads, setThreads] = useState(mockThreads);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  const addNewThread = () => {
+    if (newThread) {
+      const newThreadObj = {
+        id: threads.length + 1,
+        title: newThread,
+        content: '',
+        likesComments: '0 comments, 0 likes',
+      };
+      const updatedThreads = [...threads, newThreadObj];
+      setThreads(updatedThreads);
+      setNewThread(''); // Clear the input
+      handleClose();
+    }
   };
+
+  const addThread = () => {
+    handleShow(); // Show the modal when the button is clicked
+  };
+
+  // Define your form fields and logic for adding a new thread within the modal
 
   return (
     <div>
       <h2>Course Details</h2>
       <p>Course ID: {courseId}</p>
 
-      <button
+      <h3>Threads</h3>
+      <Button
         onClick={addThread}
+        variant="primary"
         style={{
-          position: 'absolute',
-          bottom: '15px',
-          right: '15px',
-          backgroundColor: 'blue',
-          border: 'none',
-          borderRadius: '50%',
-          width: '40px',
-          height: '40px',
-          fontSize: '30px',
-          color: 'white',
-          cursor: 'pointer',
+          marginLeft: '10px',
         }}
       >
-        +
-      </button>
-
-      <h3>Threads</h3>
+        Ask a Question
+      </Button>
       <div className="threads">
-        {mockThreads.map((thread) => (
+        {threads.map((thread) => (
           <Link key={thread.id} to={`/thread/${thread.id}`}>
             <Card key={thread.id} style={{ margin: '10px' }}>
               <Card.Body>
@@ -78,6 +91,29 @@ function CourseDetail() {
           </Link>
         ))}
       </div>
+
+      {/* New Thread Modal */}
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Thread</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <textarea
+            rows={1}
+            value={newThread}
+            onChange={(e) => setNewThread(e.target.value)}
+            placeholder="Add a new question..."
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={addNewThread}>
+            Save Question
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
